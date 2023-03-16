f18483b2007cb4bd2a0a16d19df98fe6e469efbb
lanefoxwood <lanefoxwood@gmail.com>
2023-03-16 10:27:12 +0000

    modify pingsweep

--- pingsweep.sh
+++ pingsweep.sh
@@ -3,6 +3,21 @@
 #ping sweep example which sends 5 ICMP echo packets to target the ip range of 10.11.1.0/24 or 10.11.1.0-255
 #
 
-for ip in $(seq 0 255); do
-        ping -c 5 10.11.1.$ip;
+#for ip in $(seq 0 255); do
+#        ping -c 5 10.11.1.$ip;
+#done
+#==============================================================================================#
+#!/bin/bash
+#
+#This script accepts 3 args to ping a range of ip addresses and prints only those ip addresses which respond - one per line
+#
+
+for i in $(seq $2 $3); do
+
+        ping -n -c 1 $1.$i 2>&1 >/dev/null
+        if [ $? = 0 ]
+        then
+                echo $1.$i
+        fi;
 done
+
